232c92f4bf929aebc8e5ea585c84381a3af113ee
GGURIMON <ehdrhks105@naver.com>
2024-10-13 11:46:38 +0900

    Nested Condition

--- 04_JavaScript_practice/02_Learning_JS_Grammer/app.js
+++ 04_JavaScript_practice/02_Learning_JS_Grammer/app.js
@@ -15,3 +15,16 @@
 //     console.log("Yay this is Saturday!");
 // }
 
+const password = prompt("Please enter a password");
+
+if (password.length < 8) {
+    alert("Password must be at least 8 characters long");
+}
+else {
+    if (password.indexOf(" ") > 0) {
+        alert("Password cannot contain spaces");
+    }
+    else {
+        console.log("Password has no spaces init");
+    }
+}
