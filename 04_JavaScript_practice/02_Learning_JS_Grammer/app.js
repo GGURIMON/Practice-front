de8969d5a11454b8d7c0d15113f6f3a46c8e256a
GGURIMON <ehdrhks105@naver.com>
2024-10-13 15:23:35 +0900

    asd

--- 04_JavaScript_practice/02_Learning_JS_Grammer/app.js
+++ 04_JavaScript_practice/02_Learning_JS_Grammer/app.js
@@ -21,7 +21,7 @@ if (password.length < 8) {
     alert("Password must be at least 8 characters long");
 }
 else {
-    if (password.indexOf(" ") > 0) {
+    if (password.indexOf(" ") === -1) {
         alert("Password cannot contain spaces");
     }
     else {
