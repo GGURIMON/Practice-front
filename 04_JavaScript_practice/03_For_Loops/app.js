0109271a0a5201c13d0ec57574a66223bbf515b4
GGURIMON <ehdrhks105@naver.com>
2024-10-13 19:00:09 +0900

    for loop more

--- 04_JavaScript_practice/03_For_Loops/app.js
+++ 04_JavaScript_practice/03_For_Loops/app.js
@@ -9,7 +9,19 @@
 // console.log(9);
 // console.log(10);
 
-for (let i = 1; i <= 10; i++) {
-    console.log("this is gonna count until 10");
+// for (let i = 1; i <= 10; i++) {
+//     console.log("this is gonna count until 10");
+//     console.log(i);
+// }
+
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+
+for (let i = 20; i >= 0; i = i - 2) {
     console.log(i);
 }
