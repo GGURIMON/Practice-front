4a82b79aaaaa735bad7616256dc4ee5f67e0b58a
GGURIMON <ehdrhks105@naver.com>
2024-10-13 11:35:07 +0900

    Learned Else, If, Else If

---
+++ 04_JavaScript_practice/02_Learning_JS_Grammer/app.js
@@ -0,0 +1,17 @@
+// console.log("Before If Conditional")
+// let random = Math.round(Math.random() * 10)
+// if (random > 5) {
+//     console.log(`Your Number Is ${random}`);
+//     console.log(random)
+// };
+// console.log("After If Conditional")
+
+// const dayOfWeek = "Saturday";
+
+// if (dayOfWeek === "Monday") {
+//     console.log("I hate monday");
+// }
+// else if (dayOfWeek === "Saturday") {
+//     console.log("Yay this is Saturday!");
+// }
+
